1075383304fd3987bd369adbe431257fb32b6f10
Michael Gaskin <gaskin.cet@gmail.com>
2019-02-03 14:00:13 -0800

    more pages added

--- working/field-locations.php
+++ working/field-locations.php
@@ -29,19 +29,19 @@
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#small_menu">
               <span class="glyphicon glyphicon-tasks"></span>
             </button>
-            <a class="navbar-brand" href="#">Menu</a>
+            <!--<a class="navbar-brand" href="#">Menu</a>-->
           </div>
             <div class="collapse navbar-collapse" id="small_menu" >         
               <ul class="nav navbar-nav">
-                <li><a href="http://pdqflyers.com/"><span class="glyphicon glyphicon-home"></span></a></li>
+                <li><a href="index.php"><span class="glyphicon glyphicon-home"></span></a></li>
                 <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">Club Info<span class="caret"></span></a>
                   <ul class="dropdown-menu">
-                    <li><a href="../membership.php">Membership</a></li>
-                    <li><a href="member-data-update.php">Member Updates</a></li>
-                    <li><a href="field-locations.php">Field Locations</a></li>
-                    <li><a href="pdq-rules.php">Rules</a></li>
                     <li><a href="project-about-our-club.php">About our club</a></li>
+                    <li><a href="member_info.php">Membership</a></li>
+                    <!--<li><a href="member-data-update.php">Member Updates</a></li>-->
+                    <li><a href="field-locations.php">Field Locations</a></li>
+                    <li><a href="pdq-rules.php">Rules</a></li>                    
                     <li class="divider"></li>
                     <li><a href="contact-us.php">Contact us</a></li>   
                   </ul>
@@ -51,9 +51,7 @@
                   <ul class="dropdown-menu">
                     <li><a href="email-broadcast.php"><span class="glyphicon glyphicon-envelope"></span>&nbsp;eMail</a></li>
                     <li><a href="member-data-access.php"><span class="glyphicon glyphicon-list"></span>&nbsp;Member Info</a></li>
-                    <li class="divider"></li>
-                    <li><a href="MAAC Avery5262AddressLabels.pdf"><span class="glyphicon glyphicon-file"></span>&nbsp;Model ID Label</a></li>
-                    <li><a href="indexold.php">Old Site</a></li>
+                    <li><a href="interests.php">Technical Articles</a></li>
                   </ul> 
                 </li>       
                 <li class="dropdown">
@@ -62,16 +60,19 @@
                     <li><a href="event-schedule.php"><span class="glyphicon glyphicon-calendar"></span>&nbsp;Events</a></li>
                     <li><a href="our-airfield.php"><span class="glyphicon glyphicon-film"></span>&nbsp;Multimedia</a></li>
                     <li><a href="online-swap-meet.php"><span class="glyphicon glyphicon-gift"></span>&nbsp;Market</a></li>
+                    <li class="divider"></li>
+                    <li><a href="MAAC Avery5262AddressLabels.pdf"><span class="glyphicon glyphicon-file"></span>&nbsp;Model ID Label</a></li>
                   </ul>
                 </li>
               </ul>
               <ul class="nav navbar-nav navbar-right">
+                  <li><a href="indexold.php">Old Site</a></li>
                   <li><a href="https://www.wunderground.com/personal-weather-station/dashboard?ID=INANAIMO20">Weather</a></li>
                   <li><a href="members-only.php"><span class="glyphicon glyphicon-log-in"></span>&nbsp;Login</a></li>
                 </ul>
             </div>
         </div>
- <div id="sidebar">
+ <!--<div id="sidebar">
 
     <ul>
       <li><a href="#"></a></li>
@@ -86,24 +87,58 @@
       <span></span>
       <span></span>
     </div>
-  </div>
+  </div>-->
 
       </nav>
-    <!--</div>-->
-
-   
-
-
-</div>
+  </div>
 </nav>
 
-
-
 <div id="wrapper">
-<div class= "box">
+  <div class= "box">
  
+<h3>Field Locations</h3>
 
-</div>
+<p>&nbsp;We have two flying locations: our <a href="field-locations.php#MainField">main field</a> and the <a href="field-locations.php#IndoorFlying">indoor flying location</a>.</p>
+
+<h3>Main Flying Field</h3>
+
+<p>The field is located by the green arrow.  If you use a GPS, the coordinates of the entrance gate are:</p>
+
+<p><span style="FONT-SIZE: 12pt">N49°16.441' W124°14.215'</span></p>
+
+<p><iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?q=49.274917,-124.226975&amp;num=1&amp;t=h&amp;vpsrc=6&amp;hl=en&amp;ie=UTF8&amp;ll=49.265788,-124.207048&amp;spn=0.081773,0.222988&amp;z=13&amp;output=embed"></iframe><br>
+<!--<small><a style="COLOR: #0000ff; TEXT-ALIGN: left"
+   href=
+   "http://maps.google.com/maps?q=49.274917,-124.226975&amp;num=1&amp;t=h&amp;vpsrc=6&amp;hl=en&amp;ie=UTF8&amp;ll=49.265788,-124.207048&amp;spn=0.081773,0.222988&amp;z=13&amp;source=embed">
+View Larger Map</a></small></p>-->
+
+<h3>Indoor Flying Field</h3>
+
+       <p>We have an agreement with Nanoose Place to use their building for indoor flying on Thursday evenings in the winter from 7:00pm to 9pm.  Any exceptions will be posted on the <a href="club-news.php">Club News</a> page.</p>
+       <br><br>
+
+       <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=d&amp;source=s_d&amp;saddr=&amp;daddr=49.272789,-124.192053&amp;hl=en&amp;geocode=&amp;sll=49.272831,-124.191921&amp;sspn=0.01022,0.027874&amp;vpsrc=0&amp;mra=mift&amp;mrsp=1&amp;sz=16&amp;ie=UTF8&amp;t=h&amp;ll=49.272831,-124.191921&amp;spn=0.01022,0.027874&amp;output=embed"></iframe>
+   <br><br>
+
+<h3>Weather at the Field</h3>
+
+       <p>&nbsp;We have a solar powered weather station at our outdoor flying site. Clicking on the "Weather" button on the main menu bar will take you to a Wunderground site that hosts the data from our station. The weather information includes:
+         <ol>
+            <li>Temperature</li>
+            <li>Wind Speed and Direction</li>
+            <li>Wind Direction</li>
+            <li>Barometric Pressure</li>
+            <li>Humidity</li>
+            <li>Percipitation Intensity and Accumulated</li>
+         </ol>
+       <p>The staion updates the Wunderground site every 10 minutes. There is also a widget in the footer of each page of our website that gives the latest temperature and wind velocity.</p>
+
+<h3>Field Appreciation Fridays</h3>
+
+       <p>Every Friday from 10 AM to Noon is allocated to field maintenance, improvements, and general "sprucing up". Come during this period, and help to make our field a better place to be and bring your lunch so that you can stay to fly in the afternoon. </p>
+
+
+  </div>
 </div>
 
 <footer>
--- working/index.php
+++ working/index.php
@@ -22,14 +22,13 @@
 
 <nav id="sticky-menubar">
   <div class="body-wrap">
-   <!--<div class="container">-->
       <nav class="navbar navbar-inverse">
         <div class="container-fluid">
           <div class="navbar-header" >
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#small_menu">
               <span class="glyphicon glyphicon-tasks"></span>
             </button>
-            <a class="navbar-brand" href="#">Menu</a>
+            <!--<a class="navbar-brand" href="#">Menu</a>-->
           </div>
             <div class="collapse navbar-collapse" id="small_menu" >         
               <ul class="nav navbar-nav">
@@ -38,11 +37,10 @@
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">Club Info<span class="caret"></span></a>
                   <ul class="dropdown-menu">
                     <li><a href="project-about-our-club.php">About our club</a></li>
-                    <li><a href="member_info.php">Membership</a></li>
+                    <li><a href="member_info.html">Membership</a></li>
                     <!--<li><a href="member-data-update.php">Member Updates</a></li>-->
-                    <li><a href="field-locations.php">Field Locations</a></li>
-                    <li><a href="pdq-rules.php">Rules</a></li>
-                    
+                    <li><a href="field-locations.html">Field Locations</a></li>
+                    <li><a href="pdq-rules.html">Rules</a></li>                    
                     <li class="divider"></li>
                     <li><a href="contact-us.php">Contact us</a></li>   
                   </ul>
@@ -52,28 +50,43 @@
                   <ul class="dropdown-menu">
                     <li><a href="email-broadcast.php"><span class="glyphicon glyphicon-envelope"></span>&nbsp;eMail</a></li>
                     <li><a href="member-data-access.php"><span class="glyphicon glyphicon-list"></span>&nbsp;Member Info</a></li>
-                    <li><a href="indexold.php">Old Site</a></li>
+                    <li><a href="interests.html">Technical Articles</a></li>
                   </ul> 
                 </li>       
                 <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">News<span class="caret"></span></a>
                   <ul class="dropdown-menu">
                     <li><a href="event-schedule.php"><span class="glyphicon glyphicon-calendar"></span>&nbsp;Events</a></li>
-                    <li><a href="our-airfield.php"><span class="glyphicon glyphicon-film"></span>&nbsp;Multimedia</a></li>
+                    <li><a href="slideshow.html"><span class="glyphicon glyphicon-film"></span>&nbsp;Multimedia</a></li>
                     <li><a href="online-swap-meet.php"><span class="glyphicon glyphicon-gift"></span>&nbsp;Market</a></li>
                     <li class="divider"></li>
-                    <li><a href="src/MAAC Avery5262AddressLabels.pdf"><span class="glyphicon glyphicon-file"></span>&nbsp;Model ID Label</a></li>
+                    <li><a href="MAAC Avery5262AddressLabels.pdf"><span class="glyphicon glyphicon-file"></span>&nbsp;Model ID Label</a></li>
                   </ul>
                 </li>
               </ul>
               <ul class="nav navbar-nav navbar-right">
+                  <li><a href="indexold.php">Old Site</a></li>
                   <li><a href="https://www.wunderground.com/personal-weather-station/dashboard?ID=INANAIMO20">Weather</a></li>
                   <li><a href="members-only.php"><span class="glyphicon glyphicon-log-in"></span>&nbsp;Login</a></li>
                 </ul>
             </div>
         </div>
+  <!-- <div id="sidebar">
+          <ul>
+          <li><a href="#ref1">Scale Modeling</a></li>
+          <li><a href="#ref2">E-Flight</a></li>
+          <li><a href="#ref3">Indoor Flying</a></li>
+          <li><a href="#ref4">Electric Flight Primer</a></li>
+          <li><a href="#ref5">Key Learnings</a></li>
+          <li><a href="#ref6">Helicopters and Multi Rotors</a></li>
+          <li><a href="#ref7">3D Flight</a></li>
+          <li><a href="#ref8">Model Building</a></li>
+        </ul>
+        <div id="sidebar-btn">
+          <span class="glyphicon glyphicon-tasks"></span>
+        </div>
+      </div>-->
       </nav>
-    <!--</div>-->
   </div>
 </nav>
 <!--
@@ -111,8 +124,7 @@ End of Experimental code-->
 <br>
   <p>PDQ Flyers is a model aircraft club with about 90 members.  All of us have a love of aircraft and flying, in all its forms.  This includes building - both scale and non-scale models, and flying - float planes, aerobatics, 3D and stunt flying, helicopters, and totally made up things that fly because they have an airfoil and a propeller.  One of our members flies Snoopy's dog house!!
 <br><br>
-In June, 2013, Shaw TV filmed some of our flyers in action and published the video on the "Go Island" section of their website.  It's available on YouTube</p> <iframe width="185" height="103" src="https://www.youtube.com/embed/IRUzSukrQ0M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><!--<a href="http://www.youtube.com/watch?v=IRUzSukrQ0M"target="_new">here.</a>-->
-
+In June, 2013, Shaw TV filmed some of our flyers in action and published the video on the "Go Island" section of their website.  It's available on YouTube</p> <iframe width="185" height="103" src="https://www.youtube.com/embed/IRUzSukrQ0M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>
 <br>
 
  <p>If you have an interest in airplanes or helicopters or anything else that man has built that flies, take a look at the rest of this website.  You might find that this hobby fits you!  (And don't forget the kids - building and flying is a great way to do something together and young people both love and excel at it). 
